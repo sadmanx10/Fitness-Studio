5577064940bd40521c11ed68b32786367058ac28
Iamshuvo10 <133046181+MahfujurRahmanShuvo@users.noreply.github.com>
2025-10-06 01:08:02 +0600

    trainers update

--- Fitness/admin.py
+++ Fitness/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Productz,Challenge,UserProfile
+from .models import Productz, Challenge, UserProfile, Trainer
 
 # Register your models here.
-admin.site.register([Productz,Challenge,UserProfile])
+admin.site.register([Productz, Challenge, UserProfile, Trainer])
--- Fitness/urls.py
+++ Fitness/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from .views import authView, index
+from .views import authView, index, trainers_list, trainer_detail, add_trainer, delete_trainer
 
 urlpatterns = [
  path("", index, name="index"),
  path("signup.html/", authView, name="authView"),
  path("accounts/", include("django.contrib.auth.urls")),
+	path("trainers/", trainers_list, name="trainers_list"),
+	path("trainers/<int:t_id>/", trainer_detail, name="trainer_detail"),
+	path("trainers/add/", add_trainer, name="add_trainer"),
+	path("trainers/<int:t_id>/delete/", delete_trainer, name="delete_trainer"),
 ]
